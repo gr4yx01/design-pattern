86fc881e584948d555923532cc2f18f4a7af990b
gr4yx01 <hoyx0101@gmail.com>
2025-04-19 14:53:32 +0100

    add index.ts

---
+++ typescript/index.ts
@@ -0,0 +1,35 @@
+interface Shape {
+  area(): number;
+  perimeter(): number;
+}
+
+class Circle implements Shape {
+  constructor(private radius: number) {}
+
+  area(): number {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+
+  perimeter(): number {
+    return 2 * Math.PI * this.radius;
+  }
+}
+
+class Triangle implements Shape {
+  constructor(
+    private width: number,
+    private height: number,
+  ) {}
+
+  area(): number {
+    return 0.5 * this.width * this.height;
+  }
+
+  perimeter(): number {
+    return this.width * this.height * 2;
+  }
+}
+
+const calculateArea = (shape: Shape) => {
+  return shape.area();
+};
